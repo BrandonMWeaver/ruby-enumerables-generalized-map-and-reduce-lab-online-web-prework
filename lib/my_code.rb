e98a339ab1dc067dc8d44e39116a7c1ef61df12a
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 21:56:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,11 +12,7 @@ def reduce(source_array, starting_point = 0)
   true_or_false = false
   
   source_array.each do |element|
-    if element == true || element == false
-      true_or_false = yield(starting_point, element)
-    else
     starting_point = yield(starting_point, element)
-    end
   end
   
   return starting_point
