c1cb69fa13c8ccd74bcf2e6ffbf676e24206615b
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 21:56:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,14 @@ def map(source_array)
 end
 
 def reduce(source_array, starting_point = 0)
+  true_or_false = false
   
   source_array.each do |element|
+    if element == true || element == false
+      true_or_false = yield(starting_point, element)
+    else
     starting_point = yield(starting_point, element)
+    end
   end
   
   return starting_point
