7b24c2835262b3fed0f5168c66f9c35d27e1840b
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 21:32:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
-# Your Code Here
+def map(source_array)
+  temp_array = []
+  
+  source_array.each do |element|
+    temp_array << yield(element)
+  end
+  
+  return temp_array
+end
+
+def reduce(source_array, starting_point = 0)
+  
+end
